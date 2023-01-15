9e32938ad648ed6550d002c175993e1675e5900e
Señorx <lrcvins@gmail.com>
2023-01-15 19:36:50 -0300

    cuarto

---
+++ fundamentos-programacion/while.js
@@ -0,0 +1,10 @@
+//es como for pero se usa cuando no se sabe la cantidad de iteraciones que se va a hacer durante el ciclo
+//por ejemplo para una base de datos, no se sabe si hay uno o un millon.
+
+
+var x = 10;
+
+while (x > 0) {
+    console.log("la variable vale"+x);
+    x--;
+}
